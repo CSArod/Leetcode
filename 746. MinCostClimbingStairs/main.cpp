4cbd28cd49691db6e1d322105b4f6421f3716bb7
arodcode <rodria93@unlv.nevada.edu>
2022-08-02 14:42:51 -0700

    initial leedcode #746 solution

---
+++ 746. MinCostClimbingStairs/main.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int i = 0;
+        
+        for(i = 2; i < cost.size(); i++)
+        {
+            cost[i] += min(cost[i - 1], cost[i - 2]);   
+        }
+        
+        return min(cost[cost.size() - 1], cost[cost.size() - 2]);
+ 
+    }
+};
